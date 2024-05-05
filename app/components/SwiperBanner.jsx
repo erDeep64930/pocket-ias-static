27cc7276562a62d623982b12aac9e00652f87d4a
erDeep640390 <erdeep64930@gmail.com>
2024-05-05 19:15:30 +0530

    something added

--- app/components/SwiperBanner.jsx
+++ app/components/SwiperBanner.jsx
@@ -6,116 +6,116 @@ const SwiperBanner = () => {
 
 
 
-<div class="background"></div>
-    <div class="background-texture"></div>
+<div className="background"></div>
+    <div className="background-texture"></div>
 
-    <section class="carousel">
-      <h2 class="categories__title">My List</h2>
-      <div class="carousel__container">
-        <div class="carousel-item">
+    <section className="carousel">
+      <h2 className="categories__title">My List</h2>
+      <div className="carousel__container">
+        <div className="carousel-item">
           <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/708392/pexels-photo-708392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
-              <span class="fas fa-plus-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/1785001/pexels-photo-1785001.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
             alt="people"
           />
-          <div class="carousel-item__details">
+          <div className="carousel-item__details">
             <div class="controls">
-              <span class="fas fa-play-circle"></span>
+              <span className="fas fa-play-circle"></span>
               <span class="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
-              <span class="fas fa-plus-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
             class="carousel-item__img"
             src="https://images.pexels.com/photos/1071882/pexels-photo-1071882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
-              <span class="fas fa-plus-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/417344/pexels-photo-417344.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
-              <span class="fas fa-plus-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
             class="carousel-item__img"
             src="https://images.pexels.com/photos/6945/sunset-summer-golden-hour-paul-filitchkin.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
               <span class="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
 
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
-            class="carousel-item__img"
+            className="carousel-item__img"
             src="https://images.pexels.com/photos/1964471/pexels-photo-1964471.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
             alt="people"
           />
-          <div class="carousel-item__details">
-            <div class="controls">
-              <span class="fas fa-play-circle"></span>
-              <span class="fas fa-plus-circle"></span>
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
             </div>
-            <h5 class="carousel-item__details--title">Descriptive Title</h5>
-            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
           </div>
         </div>
       </div>
