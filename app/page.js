186f114713d7d307e1b4b1f36515e5a9add4b0b3
erDeep640390 <erdeep64930@gmail.com>
2024-05-05 18:08:32 +0530

    card upadedt

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import { Banner } from "./components/Banner";
 import { MainsCategories } from "./components/MainsCategories";
 import { PrelimsCategories } from "./components/PrelimsCategories";
+import SwiperBanner from "./components/SwiperBanner";
 
 export default function Home() {
   return (
     <>
     <Banner />
+    <SwiperBanner />
     <MainsCategories />
     <PrelimsCategories />
 
